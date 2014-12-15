1e6647c104a35568618d97c453ec3c83c666a211
konrad <eiselekd@eiselekd-VirtualBox.(none)>
2014-12-15 16:07:11 +0100

    all

--- cyclone/bemicro_or1k/ip/mor1kx/mor1kx_master.v
+++ cyclone/bemicro_or1k/ip/mor1kx/mor1kx_master.v
@@ -1,6 +1,6 @@
 
 module mor1kx_master 
-  (
+(
    clk,
    rst,
    
@@ -24,26 +24,26 @@ module mor1kx_master
    
 );
    
-   input 			      clk;
-   input 			      reset;
+   input 			 clk;
+   input 			 reset;
    
-   output [31:0] 		      avm_d_address_o;
-   output [3:0] 		      avm_d_byteenable_o;
-   output 			      avm_d_read_o;
-   input [31:0] 		      avm_d_readdata_i;
-   output [3:0] 		      avm_d_burstcount_o;
-   output 			      avm_d_write_o;
-   output [31:0] 		      avm_d_writedata_o;
-   input 			      avm_d_waitrequest_i;
-   input 			      avm_d_readdatavalid_i;
+   output [31:0] 		 avm_d_address_o;
+   output [3:0] 		 avm_d_byteenable_o;
+   output 			 avm_d_read_o;
+   input [31:0] 		 avm_d_readdata_i;
+   output [3:0] 		 avm_d_burstcount_o;
+   output 			 avm_d_write_o;
+   output [31:0] 		 avm_d_writedata_o;
+   input 			 avm_d_waitrequest_i;
+   input 			 avm_d_readdatavalid_i;
    
-   output [31:0] 		      avm_i_address_o;
-   output [3:0] 		      avm_i_byteenable_o;
-   output 			      avm_i_read_o;
-   input [31:0] 		      avm_i_readdata_i;
-   output [3:0] 		      avm_i_burstcount_o;
-   input 			      avm_i_waitrequest_i;
-   input 			      avm_i_readdatavalid_i;
+   output [31:0] 		 avm_i_address_o;
+   output [3:0] 		 avm_i_byteenable_o;
+   output 			 avm_i_read_o;
+   input [31:0] 		 avm_i_readdata_i;
+   output [3:0] 		 avm_i_burstcount_o;
+   input 			 avm_i_waitrequest_i;
+   input 			 avm_i_readdatavalid_i;
 
 
    
