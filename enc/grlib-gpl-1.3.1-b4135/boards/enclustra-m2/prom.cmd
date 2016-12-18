7ee8cba03b4b1bb4691837609e2fd6af3a1f392d
Konrad Eisele <eiselekd@gmail.com>
2016-12-18 21:19:01 +0100

    all

--- enc/grlib-gpl-1.3.1-b4135/boards/enclustra-m2/prom.cmd
+++ enc/grlib-gpl-1.3.1-b4135/boards/enclustra-m2/prom.cmd
@@ -1,7 +1,7 @@
 setMode -bs
 setCable -port auto
 identify -inferir 
-attachflash -position 1 -spi "W25Q64BV"
-assignfiletoattachedflash -position 1 -file "gr-xc6s.mcs"
+attachflash -position 1 -spi "W25Q128BV"
+assignfiletoattachedflash -position 1 -file "enclustra-m2.mcs"
 program -p 1 -dataWidth 4 -spionly -e -v -loadfpga 
 quit
