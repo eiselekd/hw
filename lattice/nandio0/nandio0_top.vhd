4057e0c73f42e796d64e753a747cc68b6dc26f5d
Konrad Eisele <eiselekd@gmail.com>
2016-12-26 08:53:53 +0100

    all

--- lattice/nandio0/nandio0_top.vhd
+++ lattice/nandio0/nandio0_top.vhd
@@ -57,23 +57,24 @@ architecture rtl of nandio_top is
 
    signal data_i : std_logic_vector(31 downto 0);
    
-type nandio_states is (nandio_idle, nandio_write);
-type regs_nandio is record
-  state         : nandio_states;
-  cnt           : std_logic_vector(31 downto 0);
-  nand_cle      : std_logic;
-  nand_ale      : std_logic;
-  nand_nwe      : std_logic;
-  nand_nwp      : std_logic;
-  nand_nce      : std_logic;
-  nand_nre      : std_logic;
-  nand_data_out : std_logic_vector(15 downto 0);
-end record;
+   type nandio_states is (nandio_idle, nandio_write);
+   type regs_nandio is record
+      state         : nandio_states;
+      cnt           : std_logic_vector(31 downto 0);
+      nand_cle      : std_logic;
+      nand_ale      : std_logic;
+      nand_nwe      : std_logic;
+      nand_nwp      : std_logic;
+      nand_nce      : std_logic;
+      nand_nre      : std_logic;
+      nand_data_out : std_logic_vector(15 downto 0);
+   end record;
 
-signal r, rin   : regs_nandio;
-signal clk, rst, reset_i      : std_logic;
+   signal r, rin   : regs_nandio;
+   signal clk, rst, reset_i      : std_logic;
 
 begin
+
   clk <= clk_i;
   rst <= rst_i;
   reset_i <= rst_i;
