2f99128607e6d64ae6356d2b6938f9d20c9b2c5b
Konrad Eisele <eiselekd@gmail.com>
2019-01-26 10:17:18 +0100

    all

--- x86/state/state.v
+++ x86/state/state.v
@@ -3,3 +3,13 @@ typedef struct packed {
    reg 		    prefixseg ;  /* seg prefix override */
    reg [63:0][15:0] r ;
 } x86;
+
+`define b(x) x*8+7:x*8
+`define w(x) x*16+15:x*16
+`define l(x) x*32+31:x*32
+
+typedef struct packed {
+   reg [1:0]   md ;
+   reg [2:0]   rg ;
+   reg [2:0]   rm ;
+} modrm;
