1fa93563d5916327ea06add94680a9738f498cac
Konrad Eisele <eiselekd@gmail.com>
2019-02-03 21:12:43 +0100

    all

---
+++ x86/k.py
@@ -0,0 +1,12 @@
+from keystone import *
+
+# separate assembly instructions by ; or \n
+CODE = b"inc ecx"
+
+try:
+    # Initialize engine in X86-32bit mode
+    ks = Ks(KS_ARCH_X86, KS_MODE_32)
+    encoding, count = ks.asm(CODE)
+    print("%s = %s (number of statements: %u)" %(CODE, encoding, count))
+except KsError as e:
+    print("ERROR: %s" %e)
